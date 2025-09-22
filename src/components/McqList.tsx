e54b8687f6c57630b15a82a30343ab744d087c4e
School Galaxy <schoolgalaxy40@gmail.com>
2025-09-22 07:00:12 +0530

    removed React declartion

--- src/components/McqList.tsx
+++ src/components/McqList.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import '../assets/css/Banner.css';
 import '../assets/css/McqList.css';
