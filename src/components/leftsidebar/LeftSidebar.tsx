d631d67579c5f904ad1c676e981c4e6bf3a8ba3b
School Galaxy <schoolgalaxy40@gmail.com>
2025-09-27 10:14:29 +0530

    more quiz added.

--- src/components/leftsidebar/LeftSidebar.tsx
+++ src/components/leftsidebar/LeftSidebar.tsx
@@ -3,11 +3,30 @@ import { Link } from 'react-router-dom';
 import { useAuthenticator } from '@aws-amplify/ui-react';
 import '/src/assets/css/Home.css'; // Assuming Home.css will contain sidebar styles
 import iconHome from '/src/assets/img/icon-home.svg';
-import iconBirds from '/src/assets/img/icon-birds.svg';
-import iconAnimals from '/src/assets/img/icon-animals.svg';
-import iconAquatic from '/src/assets/img/icon-aquatic.svg';
 import iconFeedback from '/src/assets/img/icon-feedback.svg';
 import iconAbout from '/src/assets/img/icon-about.svg';
+
+// Define types for the quiz data
+interface Quiz {
+  id: string;
+  name: string;
+  displayName: string;
+  description: string;
+  route: string;
+  routePrefix: string;
+  category: string;
+  icon: string;
+  iconAlt: string;
+  active: boolean;
+  priority: number;
+}
+
+interface QuizzesData {
+  quizzes: Quiz[];
+}
+
+// Import JSON data with proper typing
+import quizzesData from '../../assets/json_data/quizzes.json';
 
 const LeftSidebar: React.FC = () => {
   const { user } = useAuthenticator();
@@ -25,26 +44,46 @@ const LeftSidebar: React.FC = () => {
           </li>
           {/* <li><Link to="/class-1-5">Class 1-5</Link></li>
           <li><Link to="/class-5-9">Class 5-9</Link></li> */}
-          <li>
-            <Link to="/quiz/birds">
-              <img src={iconBirds} alt="Birds" className="sidebar-icon" />
-              Birds Quiz
-            </Link>
-          </li>
-          <li>
-            <Link to="/quiz/animals">
-              <img src={iconAnimals} alt="Animals" className="sidebar-icon" />
-              Animals Quiz
-            </Link>
-          </li>
-          <li>
-            <Link to="/quiz/aquatic">
-              <img src={iconAquatic} alt="Aquatic" className="sidebar-icon" />
-              Aquatic Quiz
-            </Link>
-          </li>
+
+          {/* Dynamic quiz links generated from JSON */}
+          {(quizzesData as QuizzesData).quizzes
+            .filter((quiz: Quiz) => quiz.active)
+            .sort((a: Quiz, b: Quiz) => a.priority - b.priority)
+            .map((quiz: Quiz) => (
+            <li key={quiz.id}>
+              <Link to={quiz.route}>
+                <img
+                  src={quiz.icon}
+                  alt={quiz.iconAlt}
+                  className="sidebar-icon"
+                  onError={(e) => {
+                    // Fallback to a default icon if the specified icon fails to load
+                    const target = e.target as HTMLImageElement;
+                    if (target.src !== iconHome) {
+                      target.src = iconHome;
+                    }
+                  }}
+                />
+                {quiz.displayName}
+              </Link>
+            </li>
+          ))}
         </ul>
       </div>
+
+      {!user && (
+        <div className="widget">
+          <ul className="widget-list">
+            <li>
+              <Link to="/login" className="login-link">
+                <button className="login-button">
+                  Sign In
+                </button>
+              </Link>
+            </li>
+          </ul>
+        </div>
+      )}
       
       <div className="widget">
         {/* <div className="widget-header">Follow Us</div> */}
@@ -63,20 +102,6 @@ const LeftSidebar: React.FC = () => {
           </li>
         </ul>
       </div>
-
-      {!user && (
-        <div className="widget">
-          <ul className="widget-list">
-            <li>
-              <Link to="/login" className="login-link">
-                <button className="login-button">
-                  Sign In
-                </button>
-              </Link>
-            </li>
-          </ul>
-        </div>
-      )}
     </div>
   );
 };
