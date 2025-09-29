e36c6a00fefb83bbe2fce74b2f06039596edb132
School Galaxy <schoolgalaxy40@gmail.com>
2025-09-29 06:48:01 +0530

    updated login

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -4,8 +4,24 @@ import { defineAuth } from '@aws-amplify/backend';
  * Define and configure your auth resource
  * @see https://docs.amplify.aws/gen2/build-a-backend/auth
  */
+// export const auth = defineAuth({
+//   loginWith: {
+//     email: true,
+//   },
+// });
+
 export const auth = defineAuth({
   loginWith: {
-    email: true,
+    email: {
+      // can be used in conjunction with a customized welcome email as well
+      verificationEmailStyle: "CODE",
+      verificationEmailSubject: "Welcome to learning galaxy!",
+      verificationEmailBody: (createCode) => `Use this code to confirm your account: ${createCode()}`,
+      userInvitation: {
+        emailSubject: "Welcome to learning galaxy!",
+        emailBody: (user, code) =>
+          `We're happy to have you! You can now login with username ${user()} and temporary password ${code()}`,
+      },
+    },
   },
-});
+})
